b25790004515004b4dcd803b82c923f81b8fc6d5
DSP <95665714+Durai0226@users.noreply.github.com>
2023-04-24 22:26:02 +0530

    Adding dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://durai0226.github.io/MedicalRecov/",
+  base: "/MedicalRecov/",
 });
